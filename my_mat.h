ac41d9a7db689accdf26f0413ad359c448bcfa0e
roni-naftalovich <157619305+roni-naftalovich@users.noreply.github.com>
2024-02-12 18:57:45 +0200

    Update my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,3 +1,12 @@
-void existPath(int i, int j)
-int shortestPath(int i , int j)
-int createMatrix(int matrix[10][10])
+#ifndef MY_MAT_H
+#define MY_MAT_H
+#define maximum_weight 20
+
+#define SIZE 5
+
+void existPath(int i, int j);
+int shortestPath(int i , int j);
+int createMatrix(int matrix[10][10]);
+int KnapSack(int weight[], int value[], int selected_boll[]);
+
+#endif
